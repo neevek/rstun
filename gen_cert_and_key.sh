c799a7ab4a3db947b8c912e970cf8800f013fcda
neevek <i@neevek.net>
2024-06-08 14:53:15 +0800

    fix the script to generate self-signed certificate

--- gen_cert_and_key.sh
+++ gen_cert_and_key.sh
@@ -6,6 +6,18 @@ if [[ $domain = "" ]]; then
   exit 1
 fi
 
+openssl_cnf=
+platform=$(uname 2> /dev/null)
+
+if [[ "$platform" = "Darwin" ]]; then
+    openssl_cnf="/System/Library/OpenSSL/openssl.cnf"
+elif [[ "$platform" = "Linux" ]]; then
+    openssl_cnf="/etc/ssl/openssl.cnf"
+else
+    echo "Not supported!"
+    exit 1
+fi
+
 openssl req \
     -newkey rsa:2048 \
     -x509 \
@@ -16,7 +28,7 @@ openssl req \
     -subj /CN=$domain \
     -reqexts SAN \
     -extensions SAN \
-    -config <(cat /System/Library/OpenSSL/openssl.cnf \
-        <(printf '[SAN]\nsubjectAltName=DNS:localhost')) \
+    -config <(cat $openssl_cnf \
+        <(printf "[SAN]\nsubjectAltName=DNS:$domain")) \
     -sha256 \
     -days 3650
