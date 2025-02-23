59043cb4b9d69730d4db8e45b0ac765842aaeaa8
neevek <i@neevek.net>
2025-02-23 13:26:39 +0800

    update the script to generate certificate and bump the version

--- gen_cert_and_key.sh
+++ gen_cert_and_key.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-domain=$1
-if [[ $domain = "" ]]; then
-  echo "Usage: $0 <domain>"
+domain_or_ip=$1
+if [[ -z $domain_or_ip ]]; then
+  echo "usage: $0 <domain or ip>"
   exit 1
 fi
 
@@ -14,21 +14,27 @@ if [[ "$platform" = "Darwin" ]]; then
 elif [[ "$platform" = "Linux" ]]; then
     openssl_cnf="/etc/ssl/openssl.cnf"
 else
-    echo "Not supported!"
+    echo "not supported!"
     exit 1
+fi
+
+if [[ $domain_or_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ || $domain_or_ip =~ : ]]; then
+    san_type="IP"
+else
+    san_type="DNS"
 fi
 
 openssl req \
     -newkey rsa:2048 \
     -x509 \
     -nodes \
-    -keyout $domain.key.pem \
+    -keyout $domain_or_ip.key.pem \
     -new \
-    -out $domain.crt.pem \
-    -subj /CN=$domain \
-    -reqexts SAN \
-    -extensions SAN \
+    -out $domain_or_ip.crt.pem \
+    -subj /CN=$domain_or_ip \
+    -reqexts san \
+    -extensions san \
     -config <(cat $openssl_cnf \
-        <(printf "[SAN]\nsubjectAltName=DNS:$domain")) \
+        <(printf "[san]\nsubjectAltName=$san_type:$domain_or_ip")) \
     -sha256 \
     -days 3650
